083be93c749a4aa3679909167a35017ef07672de
Bartek Łukasik <Nexilis@users.noreply.github.com>
2018-08-21 10:36:32 +0200

    Added some automation to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,40 +1,35 @@
-#!/bin/bash
-############################
-# .make.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/dotfiles
-############################
+#!/usr/bin/env bash
 
+rm ~/.zshrc
 ln -s ~/dotfiles/.zshrc ~/.zshrc
+rm ~/.bashrc
 ln -s ~/dotfiles/.bashrc ~/.bashrc
+rm ~/.profile
 ln -s ~/dotfiles/.profile ~/.profile
+rm ~/.gitconfig
 ln -s ~/dotfiles/.gitconfig ~/.gitconfig
+rm -r ~/.oh-my-zsh
 ln -s /usr/share/oh-my-zsh ~/.oh-my-zsh
 
+FILES=$(ls -a)
+IGNORED=". .. LICENSE README.md"
 
-########## Variables
+is_ignored() {
+    local return_value="false"
+    for i in $IGNORED; do
+        if [ $1 = $i ]; then
+            local return_value="true"
+        fi
+    done
+    echo "$return_value"
+}
 
-dir=~/dotfiles                    # dotfiles directory
-olddir=~/dotfiles_old             # old dotfiles backup directory
-files=".bashrc .vimrc .vim .zshrc"    	  # list of files/folders to symlink in homedir
-
-##########
-
-# create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
-echo "...done"
-
-# change to the dotfiles directory
-echo "Changing to the $dir directory"
-cd $dir
-echo "...done"
-
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
-for file in $files; do
-    echo "Moving any existing dotfiles from ~ to $olddir"
-    mv ~/$file ~/dotfiles_old/
-    echo "Creating symlink to $file in home directory."
-    ln -s $dir/$file ~/$file
+for f in $FILES; do
+    result=$(is_ignored $f)
+    if [ $result != "true" ]; then
+        echo "$f"
+        # mv ~/$f ~/dotfiles_old
+        # rm $f
+        # ln -s $f ~/$f
+    fi
 done
-
-source ~/.bashrc
