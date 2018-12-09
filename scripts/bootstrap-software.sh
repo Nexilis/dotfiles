8851aff0dd1527e79c49ac029df33c9f953b7bf9
Bartek Łukasik <Nexilis@users.noreply.github.com>
2018-12-09 11:31:12 +0100

    Added improvements from new Manjaro installation.

--- scripts/bootstrap-software.sh
+++ scripts/bootstrap-software.sh
@@ -2,25 +2,26 @@
 
 pacman -S yay
 pacman -S vim
-pacman -S atom
-pacman -S fish
+#pacman -S fish
 pacman -S ripgrep
 pacman -S mc
 pacman -S ranger
-pacman -S neofetch
+pacman -S termite
+pacman -S neofetch           # terminal splash screen
 pacman -S doublecmd-gtk2
 pacman -S spotify
 pacman -S keepassxc
-pacman -S steam-manjaro
+#pacman -S steam-manjaro
 pacman -S telegram-desktop
 pacman -S clojure
 pacman -S dotnet-sdk
 pacman -S papirus-icon-theme
 pacman -S ttf-roboto
 pacman -S lxappearance-gtk3
-pacman -S lib32-libxrandr
-pacman -S lib32-openal
-pacman -S wmctrl
+pacman -S lib32-libxrandr    # GOG - Hyper Light Drifter on Manjaro
+pacman -S lib32-openal       # GOG - Hyper Light Drifter on Manjaro
+#pacman -S wmctrl            # xfce icon fix hack
+pacman -S aspell-en libmythes mythes-en languagetool # improved spellchecking
 yay -S oh-my-zsh-git
 yay -S google-chrome
 yay -S dropbox
@@ -28,12 +29,14 @@ yay -S simplenote-electron-bin
 yay -S skypeforlinux-stable-bin
 yay -S jetbrains-toolbox
 yay -S caprine
-yay -S freeoffice
 yay -S slack-desktop
 yay -S visual-studio-code-bin
 yay -S leiningen
 yay -S leiningen-completions
-yay -S redshift-gtk-git
+#yay -S redshift-gtk-git
 #yay -S xflux
 #yay -S xflux-gui-git
-yay -S xseticon
+#yay -S xseticon             # xfce icon fix hack
+yay -S ttf-ms-fonts
+yay -S ttf-dejavu-sans-mono-powerline-git
+yay -S otf-fira-code-git
