47dbd7e9fc5a82f5f0f309f53e03ec124d22c1d9
Bartek Łukasik <Nexilis@users.noreply.github.com>
2018-12-19 11:53:59 +0100

    Added script for installing Source Code Pro manually.

---
+++ scripts/install-source-code-pro-from-github.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+FONT_HOME=~/.local/share/fonts
+
+echo "installing fonts at $PWD to $FONT_HOME"
+mkdir -p "$FONT_HOME/adobe-fonts/source-code-pro"
+
+(git clone \
+   --branch release \
+   --depth 1 \
+   'https://github.com/adobe-fonts/source-code-pro.git' \
+   "$FONT_HOME/adobe-fonts/source-code-pro" && \
+fc-cache -f -v "$FONT_HOME/adobe-fonts/source-code-pro")
