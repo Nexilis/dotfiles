613caa5efd459401a07adf9ca41503641f9ebd3c
Bartek Łukasik <Nexilis@users.noreply.github.com>
2019-05-31 10:54:14 +0200

    Improve run scripts

--- scripts/run-insomnia.sh
+++ scripts/run-insomnia.sh
@@ -2,7 +2,7 @@
 
 . ~/dotfiles/scripts/change-app-panel-icon.sh
 
-APP="slack"
-ICON="/usr/share/pixmaps/slack.png"
+APP="insomnia"
+ICON="/usr/share/icons/hicolor/0x0/apps/insomnia.png"
 
 $APP 2> /dev/null :1 & change-app-panel-icon
