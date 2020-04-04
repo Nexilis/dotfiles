d037b99f11436815026024be17f15caa71f51807
Bartek Łukasik <Nexilis@users.noreply.github.com>
2020-04-04 13:49:14 +0200

    Improving Ubuntu bootstrap script

--- scripts/bootstrap-ubuntu.sh
+++ scripts/bootstrap-ubuntu.sh
@@ -1,48 +1,45 @@
 #!/usr/bin/env bash
 
-# 1. Install
+sudo apt install zsh xclip bat fd-find vim-gtk3 fonts-firacode -y
 
-sudo apt update & sudo apt install zsh xclip bat ripgrep fd-find vim-gtk3 -y
+echo "ripgrep"
+curl -LO https://github.com/BurntSushi/ripgrep/releases/download/12.0.1/ripgrep_12.0.1_amd64.deb
+sudo dpkg -i ripgrep_12.0.1_amd64.deb
 
-## https://ohmyz.sh/
-sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+echo "config"
+cp -f -r ../home/. ~/
+cp -f -r ../.config/mc ~/.config/mc
+cp -f -r ../.config/.vim ~/.config/.vim
+chsh -s `which zsh`
 
-## Broot
+echo "broot"
 wget https://dystroy.org/broot/download/x86_64-linux/broot -P $HOME/Downloads
 sudo mv ~/Downloads/broot /usr/local/bin/broot
 sudo chmod +x /usr/local/bin/broot
 
-## Micro
+echo "micro"
 cd /usr/local/bin; curl https://getmic.ro | sudo bash
 
-## diff-so-fancy
+echo "diff-so-fancy"
 wget https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy -P $HOME/Downloads
 sudo mv ~/Downloads/diff-so-fancy /usr/local/bin
 sudo chmod +x /usr/local/bin/diff-so-fancy
 
-## exa
+echo "exa"
 wget https://github.com/ogham/exa/releases/download/v0.9.0/exa-linux-x86_64-0.9.0.zip -P $HOME/Downloads
 unzip exa-linux-x86_64-0.9.0.zip
 sudo mv ~/Downloads/exa-linux-x86_64 /usr/local/bin/exa
 sudo chmod +x /usr/local/bin/exa
 
-## fzf
+echo "fzf"
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
-## dotnet
+echo "dotnet"
 wget https://packages.microsoft.com/config/ubuntu/19.10/packages-microsoft-prod.deb -P $HOME/Downloads
 sudo dpkg -i $HOME/Downloads/packages-microsoft-prod.deb
 sudo apt update & sudo apt install apt-transport-https dotnet-sdk-3.1 fsharp -y
 
+echo "https://ohmyz.sh/"
+sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 
-# 2. Configure
-
-cp -f -r -s ../home/. ~/
-cp -f -r -s ../.config/mc ~/.config/mc
-cp -f -r -s ../.config/.vim ~/.config/.vim
-chsh -s `which zsh`
-
-# 3. Fonts
-
-sudo apt install fonts-firacode -y
