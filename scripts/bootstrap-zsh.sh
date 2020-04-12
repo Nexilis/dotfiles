ece8f716eb852956e5f6b83015c675d975111f85
Bartek Łukasik <Nexilis@users.noreply.github.com>
2020-04-12 19:06:47 +0200

    Echos for zsh

--- scripts/bootstrap-zsh.sh
+++ scripts/bootstrap-zsh.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+echo "zsh"
 sudo apt install zsh
 
 echo "https://ohmyz.sh/"
@@ -7,9 +8,10 @@ wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -
 sh $HOME/Downloads/install.sh --keep-zshrc --skip-chsh --unattended
 rm -rf ~/Downloads/install.sh
 
-# Installs plugins
+echo "zsh plugins"
 git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone --depth 1 https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 chmod +x ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 
+echo "chsh zsh"
 chsh -s $(which zsh)
