615ff6fdc1b91ccdad6d7a5f8592afa3e42d7dc2
Bartek Łukasik <Nexilis@users.noreply.github.com>
2020-04-12 19:56:34 +0200

    Small fixes/improvements

--- scripts/bootstrap-ubuntu.sh
+++ scripts/bootstrap-ubuntu.sh
@@ -109,4 +109,3 @@ wget https://sh.rustup.rs -O $HOME/Downloads/rustup-init.sh
 chmod +x $HOME/Downloads/rustup-init.sh
 ~/Downloads/rustup-init.sh -q -y
 rm -rf $HOME/Downloads/rustup-init.sh
-rustup -V
