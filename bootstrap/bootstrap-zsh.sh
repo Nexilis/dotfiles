bbb613d7f8d52cfee01f5b1ee5b81f10b68c0713
Bartek Łukasik <Nexilis@users.noreply.github.com>
2020-04-13 14:42:37 +0200

    Switch to antigen from omz

--- bootstrap/bootstrap-zsh.sh
+++ bootstrap/bootstrap-zsh.sh
@@ -3,15 +3,12 @@
 echo "zsh"
 sudo apt install zsh -y
 
-echo "https://ohmyz.sh/"
-wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -P $HOME/Downloads
-sh $HOME/Downloads/install.sh --keep-zshrc --skip-chsh --unattended
-rm -rf ~/Downloads/install.sh
+echo "zsh antigen"
+mkdir ~/.zsh
+wget git.io/antigen -O $HOME/.zsh/antigen.zsh
 
-echo "zsh plugins"
-git clone --depth 1 https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-git clone --depth 1 https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-chmod +x ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+echo "zsh rc"
+cp -f -r ../home/.zshrc ~/
 
 echo "chsh zsh"
 chsh -s $(which zsh)
