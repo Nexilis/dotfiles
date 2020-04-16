caf48e414ace0270d136185ad7196c3634e454ba
Bartek Łukasik <Nexilis@users.noreply.github.com>
2020-04-16 16:02:29 +0200

    Nnn addition

---
+++ bootstrap/nnn.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+echo "nnn"
+wget https://github.com/jarun/nnn/releases/download/v3.1/nnn-static-3.1.x86-64.tar.gz -O $HOME/Downloads/nnn.tar.gz
+tar -xvzf $HOME/Downloads/nnn.tar.gz nnn-static
+sudo mv nnn-static /usr/local/bin/nnn
+rm -rf ~/Downloads/nnn.tar.gz
