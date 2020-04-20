9dde95f7a2c3d1525ac81f67a69da3cb148c474e
Bartek Łukasik <Nexilis@users.noreply.github.com>
2020-04-20 18:39:03 +0200

    Alacritty installation script fix

--- bootstrap/alacritty.sh
+++ bootstrap/alacritty.sh
@@ -2,7 +2,8 @@
 
 echo "alacritty"
 sudo add-apt-repository ppa:mmstick76/alacritty
-sudo apt install alacritty
+sudo apt update
+sudo apt install alacritty -y
 rm -rf ~/.config/alacritty
 cp -f -r ../.config/alacritty ~/.config/alacritty
 
