ca163d5c6e63373b6aec1b241cff71d1e646a470
Bartek Łukasik <Nexilis@users.noreply.github.com>
2020-10-03 18:25:09 +0200

    Neovim addition

--- bootstrap/update-configs.sh
+++ bootstrap/update-configs.sh
@@ -4,22 +4,33 @@ echo "zsh omz -> antigen"
 rm -rf ~/.oh-my-zsh
 mkdir -p ~/.zsh
 wget git.io/antigen -O $HOME/.zsh/antigen.zsh
+
 echo "zsh rc"
 cp -f -r ../home/.zshrc ~/
+
 echo "config mc"
 rm -rf ~/.config/mc
 cp -f -r ../.config/mc ~/.config/mc
+
 echo "config vim"
 rm -rf ~/_vimrc
 rm -rf ~/.vimrc
 cp -f -r ../.config/.vim ~/.config/.vim
 ln -s ~/proj/dotfiles/home/.vimrc ~/.vimrc
+
+echo "config neovim"
+cp -f -r ../.config/nvim ~/.config/nvim
+rm -f ~/.config/nvim/init.vim
+ln -s ~/proj/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
+
 echo "config git"
 cp -f -r ../home/.gitconfig ~/
 #cp -f -r ../home/.gitconfig-github ~/
 #cp -f -r ../home/.gitconfig-work ~/
+
 echo "config alacritty"
 rm -rf ~/.config/alacritty
 cp -f -r ../.config/alacritty ~/.config/alacritty
+
 echo "config conky"
 cp -f -r ../home/.conkyrc ~/
