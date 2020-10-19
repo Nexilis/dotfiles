a9c00cd4d9b17aee3a7f8a6d66ed8764b6345c22
Bartek Łukasik <Nexilis@users.noreply.github.com>
2020-10-19 08:16:43 +0200

    More scripts extraction & update tweaks

--- bootstrap/bootstrap-ubuntu.sh
+++ bootstrap/bootstrap-ubuntu.sh
@@ -3,24 +3,24 @@
 sudo apt install software-properties-common curl apt-transport-https xclip fd-find mc htop fonts-firacode -y
 
 sh zsh.sh
+sh fzf.sh
 sh ripgrep.sh
 sh bat.sh
 sh broot.sh
 sh nvim.sh
 sh micro.sh
+sh diff-so-fancy.sh
+sh joplin.sh
 sh jdk-programming.sh
 sh dotnet-programming.sh
 sh js-programming.sh
 sh rust-programming.sh
 
-echo "fzf"
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-~/.fzf/install --all
-
 echo "config mc"
 cp -f -r ../.config/mc ~/.config/mc
 
 echo "config vim"
+rm -rf ~/.config/vim
 cp -f -r ../.config/.vim ~/.config/.vim
 cp -f -r ../home/.vimrc ~/
 
@@ -29,18 +29,10 @@ cp -f -r ../home/.gitconfig ~/
 cp -f -r ../home/.gitconfig-github ~/
 cp -f -r ../home/.gitconfig-work ~/
 
-echo "diff-so-fancy"
-wget https://raw.githubusercontent.com/so-fancy/diff-so-fancy/master/third_party/build_fatpack/diff-so-fancy -P $HOME/Downloads
-sudo mv ~/Downloads/diff-so-fancy /usr/local/bin
-sudo chmod +x /usr/local/bin/diff-so-fancy
-
 echo "lazygit"
 sudo add-apt-repository ppa:lazygit-team/release
 sudo apt update
 sudo apt install lazygit -y
-
-echo "joplin"
-wget -O - https://raw.githubusercontent.com/laurent22/joplin/master/Joplin_install_and_update.sh | bash
 
 echo "insomnia"
 echo "deb https://dl.bintray.com/getinsomnia/Insomnia /" \
