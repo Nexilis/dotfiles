cc35371361993503112b831a9970cc5e003e3177
Bartek Łukasik <Nexilis@users.noreply.github.com>
2020-10-31 22:50:25 +0100

    Config coping fix

--- bootstrap/update.sh
+++ bootstrap/update.sh
@@ -10,17 +10,17 @@ cp -f -r ../home/.zshrc ~/
 
 echo "config mc"
 rm -rf ~/.config/mc
-cp -f -r ../.config/mc ~/.config/mc
+cp -f -r ../.config/mc ~/.config/
 
 echo "config vim"
 rm -rf ~/_vimrc
 rm -rf ~/.vimrc
 rm -rf ~/.config/vim
-cp -f -r ../.config/.vim ~/.config/.vim
+cp -f -r ../.config/.vim ~/.config/
 ln -s ~/proj/dotfiles/home/.vimrc ~/.vimrc
 
 echo "config neovim"
-cp -f -r ../.config/nvim ~/.config/nvim
+cp -f -r ../.config/nvim ~/.config/
 rm -rf ~/.config/nvim/init.vim
 rm -rf ~/.config/nvim/plugged/fzf/bin
 ln -s ~/proj/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
@@ -32,7 +32,10 @@ cp -f -r ../home/.gitconfig ~/
 
 echo "config alacritty"
 rm -rf ~/.config/alacritty
-cp -f -r ../.config/alacritty ~/.config/alacritty
+cp -f -r ../.config/alacritty ~/.config/
+
+echo "config doublecmd"
+cp -f -r ../.config/doublecmd ~/.config/
 
 sh ripgrep.sh
 sh bat.sh
