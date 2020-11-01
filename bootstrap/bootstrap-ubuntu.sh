096df337a58ac61df1c5a108553413d5cb441657
Bartek Łukasik <Nexilis@users.noreply.github.com>
2020-11-01 09:29:31 +0100

    Moving of Ubuntu specific ripgrep installation back to bootstrap-ubuntu.sh

--- bootstrap/bootstrap-ubuntu.sh
+++ bootstrap/bootstrap-ubuntu.sh
@@ -64,3 +64,8 @@ sudo add-apt-repository --yes https://adoptopenjdk.jfrog.io/adoptopenjdk/deb/
 sudo apt-get update
 sudo apt-get install adoptopenjdk-14-hotspot rlwrap -y
 sh jdk-programming.sh
+
+echo "ripgrep"
+wget https://github.com/BurntSushi/ripgrep/releases/download/12.1.1/ripgrep_12.1.1_amd64.deb -P $HOME/Downloads
+sudo dpkg -i $HOME/Downloads/ripgrep_12.1.1_amd64.deb
+rm -rf $HOME/Downloads/ripgrep_12.1.1_amd64.deb
