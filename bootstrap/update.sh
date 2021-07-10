52233e886a4d3f57419a24ea42ea5e7c12672722
Bartek Łukasik <Nexilis@users.noreply.github.com>
2021-07-10 15:49:18 +0200

    Update update ;)

--- bootstrap/update.sh
+++ bootstrap/update.sh
@@ -1,43 +1,13 @@
 #!/usr/bin/env bash
 
-echo "transit zsh omz -> to antigen"
-rm -rf ~/.oh-my-zsh
-mkdir -p ~/.zsh
-wget git.io/antigen -O $HOME/.zsh/antigen.zsh
-
-echo "zsh rc"
-cp -f -r ../home/.zshrc ~/
-
-echo "config mc"
-rm -rf ~/.config/mc
-cp -f -r ../.config/mc ~/.config/
-
-echo "config vim"
+echo "remove vim config"
 rm -rf ~/_vimrc
 rm -rf ~/.vimrc
 rm -rf ~/.config/vim
-cp -f -r ../.config/.vim ~/.config/
-ln -s ~/proj/dotfiles/home/.vimrc ~/.vimrc
 
-echo "config neovim"
-cp -f -r ../.config/nvim ~/.config/
-rm -rf ~/.config/nvim/init.vim
-rm -rf ~/.config/nvim/plugged/fzf/bin
-ln -s ~/proj/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
-
-echo "config git"
-cp -f -r ../home/.gitconfig ~/
-#cp -f -r ../home/.gitconfig-github ~/
-#cp -f -r ../home/.gitconfig-work ~/
-
-echo "config alacritty"
+echo "remove alacritty config"
 rm -rf ~/.config/alacritty
-cp -f -r ../.config/alacritty ~/.config/
 
-echo "config doublecmd"
-cp -f -r ../.config/doublecmd ~/.config/
-
-sh ripgrep.sh
-sh bat.sh
-sh broot.sh
-sh micro.sh
+echo "config kitty"
+rm -rf ~/.config/kitty
+cp -f -r ../.config/kitty ~/.config/
