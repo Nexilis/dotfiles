f7cafa64e474627568e9914ba6328c4b839ab3fa
Bartek Łukasik <Nexilis@users.noreply.github.com>
2021-07-10 15:46:33 +0200

    Remove unused software (vim, alacritty & archive)

--- bootstrap/alacritty.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo "alacritty"
-sudo add-apt-repository ppa:mmstick76/alacritty
-sudo apt update
-sudo apt install alacritty -y
-rm -rf ~/.config/alacritty
-cp -f -r ../.config/alacritty ~/.config/alacritty
-
--- bootstrap/archive/nnn.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo "nnn"
-wget https://github.com/jarun/nnn/releases/download/v3.1/nnn-static-3.1.x86-64.tar.gz -O $HOME/Downloads/nnn.tar.gz
-tar -xvzf $HOME/Downloads/nnn.tar.gz nnn-static
-sudo mv nnn-static /usr/local/bin/nnn
-rm -rf ~/Downloads/nnn.tar.gz
-curl -Ls https://raw.githubusercontent.com/jarun/nnn/master/plugins/getplugs | sh
-
