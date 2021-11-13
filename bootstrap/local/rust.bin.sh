6c15b1b4d33414bb712e69655c78233eacbf44bc
Bartek Łukasik <Nexilis@users.noreply.github.com>
2021-11-13 13:45:38 +0100

    Replace files in one step and executable bit only for user

--- bootstrap/local/rust.bin.sh
+++ bootstrap/local/rust.bin.sh
@@ -8,6 +8,5 @@ rm -f $HOME/Downloads/rustup-init.sh
 echo "rust-analyzer - LSP for rust used in neovim"
 wget https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-x86_64-unknown-linux-gnu.gz -O $HOME/Downloads/rust-analyzer.gz
 gunzip $HOME/Downloads/rust-analyzer.gz
-chmod +x $HOME/Downloads/rust-analyzer
-rm -f $HOME/.local/bin/rust-analyzer
-mv $HOME/Downloads/rust-analyzer $HOME/.local/bin/rust-analyzer
+chmod u+x $HOME/Downloads/rust-analyzer
+mv -f $HOME/Downloads/rust-analyzer $HOME/.local/bin/rust-analyzer
