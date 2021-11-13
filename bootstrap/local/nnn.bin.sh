ab98db5e68558fdfa2e2617188918e0ec1480a76
Bartek Łukasik <Nexilis@users.noreply.github.com>
2021-11-13 12:17:49 +0100

    Gocryptfs

--- bootstrap/local/nnn.bin.sh
+++ bootstrap/local/nnn.bin.sh
@@ -2,6 +2,7 @@
 
 wget https://github.com/jarun/nnn/releases/download/v4.3/nnn-static-4.3.x86_64.tar.gz -O $HOME/Downloads/nnn.tar.gz
 tar xvzf $HOME/Downloads/nnn.tar.gz -C $HOME/Downloads
+rm -f $HOME/.local/bin/nnn
 mv $HOME/Downloads/nnn-static $HOME/.local/bin/nnn
 chmod +x $HOME/.local/bin/nnn
 rm -f $HOME/Downloads/nnn.tar.gz
