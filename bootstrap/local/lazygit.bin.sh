baafbf8b2823da1887af88e0a89c91dacad565cf
Bartek Łukasik <Nexilis@users.noreply.github.com>
2021-11-13 18:39:21 +0100

    Make dir -p

--- bootstrap/local/lazygit.bin.sh
+++ bootstrap/local/lazygit.bin.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 wget https://github.com/jesseduffield/lazygit/releases/download/v0.31.3/lazygit_0.31.3_Linux_x86_64.tar.gz -O $HOME/Downloads/lazygit.tar.gz
-mkdir $HOME/Downloads/lazygit
+mkdir -p $HOME/Downloads/lazygit
 tar xvzf $HOME/Downloads/lazygit.tar.gz -C $HOME/Downloads/lazygit
 mv -f $HOME/Downloads/lazygit/lazygit $HOME/.local/bin/lg
 chmod u+x $HOME/.local/bin/lg
