9b616d7234bd6b129a70b65167298b99cc6e1e98
Bartek Łukasik <Nexilis@users.noreply.github.com>
2021-11-13 12:06:43 +0100

    Nnn

---
+++ bootstrap/local/nnn.bin.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+wget https://github.com/jarun/nnn/releases/download/v4.3/nnn-static-4.3.x86_64.tar.gz -O $HOME/Downloads/nnn.tar.gz
+tar xvzf $HOME/Downloads/nnn.tar.gz -C $HOME/Downloads
+mv $HOME/Downloads/nnn-static $HOME/.local/bin/nnn
+chmod +x $HOME/.local/bin/nnn
+rm -f $HOME/Downloads/nnn.tar.gz
