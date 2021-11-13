df3dceac7615afc6c09ff824aff1ffae8614ad19
Bartek Łukasik <Nexilis@users.noreply.github.com>
2021-11-13 13:43:46 +0100

    Neovim

---
+++ bootstrap/local/nvim.bin.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+wget https://github.com/neovim/neovim/releases/download/stable/nvim-linux64.tar.gz -O $HOME/Downloads/nvim.tar.gz
+tar xvzf $HOME/Downloads/nvim.tar.gz -C $HOME/Downloads
+cp -fur $HOME/Downloads/nvim-linux64/* $HOME/.local
+chmod u+x $HOME/.local/bin/nvim
+rm -rf $HOME/Downloads/nvim*
