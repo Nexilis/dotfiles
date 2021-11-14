27b8d08b45cf1278928b66dcc63ccf6b67baf92c
Bartek Łukasik <Nexilis@users.noreply.github.com>
2021-11-14 06:47:53 +0100

    Squasing Ubuntu specific commands into one file

--- bootstrap/js-programming.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo "nodejs"
-curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-sudo apt update && sudo apt install nodejs tidy -y
-sudo npm -g install js-beautify
-
