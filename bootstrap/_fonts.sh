be8b52fe8ca55d0a4aaa52ed8f97a128321d97bd
Bartek Łukasik <Nexilis@users.noreply.github.com>
2021-11-14 20:44:22 +0100

    Fix fonts installation

--- bootstrap/_fonts.sh
+++ bootstrap/_fonts.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
 echo "JetBrains Mono"
-wget https://github.com/JetBrains/JetBrainsMono/releases/download/v2.225/JetBrainsMono-2.225.zip -O $HOME/Downloads/JetBrainsMono.zip
-unzip -vo $HOME/Downloads/JetBrainsMono.zip -d $HOME/Downloads/JetBrainsMono
+wget https://github.com/JetBrains/JetBrainsMono/releases/download/v2.242/JetBrainsMono-2.242.zip -O $HOME/Downloads/JetBrainsMono.zip
+unzip $HOME/Downloads/JetBrainsMono.zip -d $HOME/Downloads/JetBrainsMono
 mkdir -vp $HOME/.local/share/fonts/jetbrains-mono
-cp -vfur ~/Downloads/JetBrainsMono/fonts/ttf/*.ttf $HOMW/.local/share/fonts/jetbrains-mono
+find $HOME/Downloads/JetBrainsMono/fonts/variable -maxdepth 1 -iname '*.ttf' -exec cp -vfu {} $HOME/.local/share/fonts/jetbrains-mono \;
 rm -vrf $HOME/Downloads/JetBrainsMono*
 
 echo "Cascadia Code PL"
 wget https://github.com/microsoft/cascadia-code/releases/download/v2110.31/CascadiaCode-2110.31.zip -O $HOME/Downloads/CascadiaCode.zip
-unzip -vo $HOME/Downloads/CascadiaCode.zip -d $HOME/Downloads/CascadiaCode
+unzip $HOME/Downloads/CascadiaCode.zip -d $HOME/Downloads/CascadiaCode
 mkdir -vp $HOME/.local/share/fonts/cascadia-code
-cp -vfur ~/Downloads/CascadiaCode/fonts/ttf/*.ttf $HOMW/.local/share/fonts/cascadia-code
+find $HOME/Downloads/CascadiaCode/ttf -maxdepth 1 -iname '*.ttf' -exec cp -vfu {} $HOME/.local/share/fonts/cascadia-code \;
 rm -vrf $HOME/Downloads/CascadiaCode*
 
 fc-cache -vf
