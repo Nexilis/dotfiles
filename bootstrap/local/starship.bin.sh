c205383c98b112ab22edc0ff01b73c0ba7536121
Bartek Łukasik <Nexilis@users.noreply.github.com>
2021-11-16 11:01:23 +0100

    Local starship installation

--- bootstrap/local/starship.bin.sh
+++ bootstrap/local/starship.bin.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
 echo "starship.rs"
-curl -fsSL https://starship.rs/install.sh | bash
+wget https://starship.rs/install.sh -O $HOME/Downloads/install.sh
+rm -vf $HOME/.local/bin/starship
+sh $HOME/Downloads/install.sh --force -b $HOME/.local/bin
+rm -vf $HOME/Downloads/install.sh
+
