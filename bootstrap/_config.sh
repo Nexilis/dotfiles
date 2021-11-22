68375eb4c6de4a5352554b87418a51a8dcabd0c9
Bartek Łukasik <Nexilis@users.noreply.github.com>
2021-11-22 21:35:14 +0100

    Addition of doublecmd installation & config

--- bootstrap/_config.sh
+++ bootstrap/_config.sh
@@ -9,12 +9,12 @@ chsh -s $(which zsh)
 echo "config kitty"
 rm -vrf $HOME/.config/kitty
 mkdir -vp $HOME/.config/kitty
-cp -vfr $HOME/proj/dotfiles/config/kitty $HOME/.config/kitty
+cp -vfr $HOME/proj/dotfiles/config/kitty $HOME/.config
 
 echo "config neovim"
 rm -vrf $HOME/.config/nvim
 mkdir -vp $HOME/.config/nvim
-cp -vfr $HOME/proj/dotfiles/config/nvim $HOME/.config/
+cp -vfr $HOME/proj/dotfiles/config/nvim $HOME/.config
 echo "neovim python support"
 pip3 install --user pynvim
 
@@ -22,3 +22,9 @@ echo "config git"
 cp -vf $HOME/proj/dotfiles/home/.gitconfig $HOME/
 cp -vf $HOME/proj/dotfiles/home/.gitconfig-github $HOME/
 cp -vf $HOME/proj/dotfiles/home/.gitconfig-work $HOME/
+
+echo "doublecmd"
+rm -vf $HOME/.local/share/doublecmd/doublecmd.inf
+rm -vrf $HOME/.config/doublecmd
+mkdir -vp $HOME/.config/doublecmd
+cp -vfr $HOME/proj/dotfiles/config/doublecmd $HOME/.config
