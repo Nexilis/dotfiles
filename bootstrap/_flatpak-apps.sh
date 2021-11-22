be0f3db39f9e0ea7c601495a2a84f5b5e6b421f3
Bartek Łukasik <Nexilis@users.noreply.github.com>
2021-11-22 09:48:18 +0100

    Seperate flatpaks for work

--- bootstrap/_flatpak-apps.sh
+++ bootstrap/_flatpak-apps.sh
@@ -4,19 +4,14 @@ flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/fl
 
 flatpak install flathub org.libreoffice.LibreOffice -y
 flatpak install flathub net.cozic.joplin_desktop -y
-flatpak install flathub rest.insomnia.Insomnia -y
 flatpak install flathub com.spotify.Client -y
 flatpak install flathub com.bitwarden.desktop -y
-flatpak install flathub org.gaphor.Gaphor -y
 flatpak install flathub org.gimp.GIMP -y
 flatpak install flathub com.github.tchx84.Flatseal -y
 flatpak install flathub org.gnome.meld -y
 flatpak install flathub org.gnome.gitg -y
+flatpak install flathub org.gnome.Extensions -y
 flatpak install flathub org.telegram.desktop -y
 flatpak install flathub com.sindresorhus.Caprine -y
-
-flatpak install flathub com.slack.Slack -y
-flatpak install flathub us.zoom.Zoom -y
-
 flatpak install flathub de.haeckerfelix.Fragments -y
-flatpak install flathub org.gnome.Extensions -y
+flatpak install flathub fr.romainvigier.MetadataCleaner -y
