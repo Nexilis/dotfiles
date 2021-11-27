a68270a8b005fca58925e2766f9c8bf77ea7e8d5
Bartek Łukasik <Nexilis@users.noreply.github.com>
2021-11-27 22:52:19 +0100

    Flatpak & Fedora installation scripts improvements

--- bootstrap/_flatpak-apps.sh
+++ bootstrap/_flatpak-apps.sh
@@ -8,8 +8,10 @@ flatpak install flathub com.spotify.Client -y
 flatpak install flathub com.bitwarden.desktop -y
 flatpak install flathub org.gimp.GIMP -y
 flatpak install flathub com.github.tchx84.Flatseal -y
+flatpak install flathub org.gnome.TextEditor -y
 flatpak install flathub org.gnome.meld -y
 flatpak install flathub org.gnome.gitg -y
 flatpak install flathub org.telegram.desktop -y
 flatpak install flathub de.haeckerfelix.Fragments -y
 flatpak install flathub fr.romainvigier.MetadataCleaner -y
+flatpak install flathub org.videolan.VLC -y
--- bootstrap/bootstrap-ubuntu.sh
+++ bootstrap/bootstrap-ubuntu.sh
@@ -1,31 +1,26 @@
 #!/usr/bin/env bash
 
-sudo apt install zsh software-properties-common curl apt-transport-https xclip kitty imagemagick -y
+sudo apt install zsh software-properties-common curl apt-transport-https kitty imagemagick -y
 
 sh _local.sh
 sh _config.sh
 sh _fonts.sh
 
-echo "python & pip"
-sudo apt update
+echo "brave"
+curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt update -y && sudo apt install brave-browser -y
+
+echo "python3"
+sudo apt update -y
 sudo apt install python3 python3-pip -y
 
 echo "nodejs"
 curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-sudo apt update && sudo apt install nodejs tidy -y
+sudo apt update -y && sudo apt install nodejs tidy -y
 sudo npm -g install js-beautify
 
-echo "lua 5.3 - latest compatible with Ubuntu 20.04 + luarocks - package manager"
-sudo apt update
-sudo apt install lua5.3 liblua5.3-dev libreadline-dev luarocks -y
-
-echo "fennel - Lisp on lua"
-sudo rm -rf /usr/local/bin/fennel
+echo "lua"
+sudo apt update -y && sudo apt install lua5.3 liblua5.3-dev libreadline-dev luarocks -y
 luarocks install --local fennel
 luarocks install --local readline
-
-echo "brave"
-curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
-echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-sudo apt update
-sudo apt install brave-browser -y
