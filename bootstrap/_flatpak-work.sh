fe5ba062ace3cb1aca933d99ee782009d4813c67
Bartek Łukasik <Nexilis@users.noreply.github.com>
2022-03-15 17:01:30 +0100

    Remove Wireshark (permission probs in flatpak version), add Minder

--- bootstrap/_flatpak-work.sh
+++ bootstrap/_flatpak-work.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 flatpak install flathub rest.insomnia.Insomnia -y
+flatpak install flathub com.github.phase1geo.minder -y
 flatpak install flathub com.slack.Slack -y
 flatpak install flathub us.zoom.Zoom -y
 flatpak install flathub org.gaphor.Gaphor -y
-flatpak install flathub org.wireshark.Wireshark -y
 flatpak install flathub com.uploadedlobster.peek -y
