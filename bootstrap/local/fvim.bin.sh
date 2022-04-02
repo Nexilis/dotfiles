ec5c147d32c8ace799819b692b487b6948802c8f
Bartek Łukasik <Nexilis@users.noreply.github.com>
2022-04-02 14:16:55 +0200

    Install/ config nvui (replace fvim)

--- bootstrap/local/fvim.bin.sh
+++
@@ -1,2 +0,0 @@
-# WIP
-# sudo dnf install fvim-linux-x86_64-v0.3.528+g5190942.rpm
--- config/nvim/lua/settings.lua
+++ config/nvim/lua/settings.lua
@@ -1,3 +1,6 @@
+local set = vim.opt
+set.termguicolors = true
+
 -- Configure LSP
 -- https://github.com/neovim/nvim-lspconfig#rust_analyzer
 -- nvim_lsp object
@@ -10,18 +13,27 @@ end
 nvim_lsp.rust_analyzer.setup({ on_attach=on_attach })
 -- Enable diagnostics
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics, {
-    virtual_text = true,
-    signs = true,
-    update_in_insert = true,
-  }
-)
+    vim.lsp.diagnostic.on_publish_diagnostics, {
+        virtual_text = true,
+        signs = true,
+        update_in_insert = true,
+    })
 require'nvim-web-devicons'.setup {}
 require'hop'.setup()
 require'nvim-tree'.setup {}
-require('feline').setup()
-vim.opt.termguicolors = true
-require("bufferline").setup{}
-require('nvim_comment').setup()
-require('gitsigns').setup()
-print('Hello! Lua setting loaded. 12')
+require'feline'.setup()
+require'bufferline'.setup {}
+require'nvim_comment'.setup()
+require'gitsigns'.setup()
+
+if vim.g.nvui then
+    vim.cmd [[
+        NvuiFrameless 0
+        NvuiPopupMenu 1
+        NvuiCmdline 1
+        NvuiCmdFontSize 20
+        NvuiCmdFontFamily Hack Nerd Font Mono
+    ]]
+    set.guifont='Hack Nerd Font Mono:h13'
+end
+
