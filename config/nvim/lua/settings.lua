12d150cbdf7e0163b120ef99289e4806ed2bcb06
Bartek Łukasik <Nexilis@users.noreply.github.com>
2022-04-14 10:06:19 +0200

    Nvui config tweaks

--- config/nvim/lua/settings.lua
+++ config/nvim/lua/settings.lua
@@ -5,13 +5,18 @@ set.listchars:append('space:⋅')
 
 if vim.g.nvui then
     vim.cmd [[
+        NvuiAnimationsEnabled 0
         NvuiFrameless 0
         NvuiPopupMenu 1
         NvuiCmdline 1
+        NvuiCmdCenterYPos 1
+        NvuiCmdPadding 0
+        NvuiCmdBorderWidth 0
         NvuiCmdFontSize 20
+        NvuiCmdBigFontScaleFactor 1.0
         NvuiCmdFontFamily Hack Nerd Font Mono
     ]]
-    set.guifont='Hack Nerd Font Mono:h13'
+    set.guifont='Hack Nerd Font Mono:h11'
 end
 
 require'nvim-web-devicons'.setup {}
