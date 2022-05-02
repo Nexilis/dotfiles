c511f76e5e0dcb6a733dad4ac3732dc3591237e1
Bartek Łukasik <Nexilis@users.noreply.github.com>
2022-05-02 21:25:02 +0200

    Remove archive

--- bootstrap/local/_archive/micro.bin.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-echo "micro"
-rm -vf micro
-curl https://getmic.ro | bash
-mv -vf micro $HOME/.local/bin/micro
-chmod u+x $HOME/.local/bin/micro
--- bootstrap/local/_archive/nnn.bin.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-wget https://github.com/jarun/nnn/releases/download/v4.4/nnn-static-4.4.x86_64.tar.gz -O $HOME/Downloads/nnn.tar.gz
-tar -vxzf $HOME/Downloads/nnn.tar.gz -C $HOME/Downloads
-mv -vf $HOME/Downloads/nnn-static $HOME/.local/bin/nnn
-chmod u+x $HOME/.local/bin/nnn
-rm -vf $HOME/Downloads/nnn.tar.gz
