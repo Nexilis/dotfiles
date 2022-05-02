6ee617e675192d36435424905639c5cb593b2ddd
Bartek Łukasik <Nexilis@users.noreply.github.com>
2022-05-02 21:23:02 +0200

    Migrating config to lua almost done

--- config/nvim/lua/keybindings.lua
+++ config/nvim/lua/keybindings.lua
@@ -3,48 +3,17 @@ local key = vim.keymap
 
 global.mapleader=' '
 global.maplocalleader=','
--- override leader for vim-which-key
-key.set('n', '<leader>', ':<c-u>WhichKey \'<Space>\'<CR>', {silent = true})
 
 -- ctrl+z, ctrl+v
-key.set('i', '<c-z>' ,'<c-o>:u<CR>', {silent = true})
+key.set('i', '<c-z>' ,'<c-o>:u<cr>', {silent = true})
 key.set('i', '<c-v>', '<esc>:set paste<cr>a<c-r>=getreg(\'+\')<cr><esc>:set nopaste<cr>a', {silent = true})
 -- delete without copying
 key.set('n', 'x', '"_x')
 key.set({'n', 'v'}, 'd', '"_d')
 key.set('n', 'D', '"_D')
 
-key.set('n', '<leader>t', ':NvimTreeToggle<CR>')
-key.set('n', '<leader>u', ':UndotreeToggle<CR>')
-key.set('n', '<leader>k', ':bnext<CR>')
-key.set('n', '<leader>j', ':bprevious<CR>')
-key.set('n', '<leader>q', ':qa!<CR>')
-key.set('n', '<leader>os', ':setlocal spell! spelllang=en_us<CR>')
-key.set('n', '<leader>or', ':so $MYVIMRC<CR>')
-key.set('n', '<leader>of', ':Neoformat<CR>')
-key.set('n', '<leader>ow', ':ToggleWorkspace<CR>')
-key.set('n', '<leader>oa', ':ToggleAutosave<CR>')
-key.set('n', '<leader>bw', ':w!<CR>')
-key.set('n', '<leader>bx', ':bp <BAR> bd! #<CR>')
-key.set('n', '<leader>bn', ':enew<CR>')
-key.set('n', '<leader>wq', '<C-w>q')
-key.set('n', '<leader>ws', ':split<cr>')
-key.set('n', '<leader>wv', ':vsplit<cr>')
-key.set('n', '<leader>wo', ':only<cr>')
-key.set('n', '<leader>wh', '<C-w>h')
-key.set('n', '<leader>wj', '<C-w>j')
-key.set('n', '<leader>wk', '<C-w>k')
-key.set('n', '<leader>wl', '<C-w>l')
-key.set('n', '<leader>sc', ':set syn=cs<cr>')
-key.set('n', '<leader>sf', ':set syn=fsharp<cr>')
-key.set('n', '<leader>sj', ':set syn=javascript<cr>')
-key.set('n', '<leader>st', ':set syn=typescript<cr>')
-key.set('n', '<leader>ss', ':set syn=json<cr>')
-key.set('n', '<leader>sx', ':set syn=xml<cr>')
-key.set('n', '<leader>sb', ':set syn=sh<cr>')
-key.set('n', '<leader>sm', ':set syn=terrafrom<cr>')
-key.set('n', '<leader>sr', ':set syn=rust<cr>')
-key.set('n', '<leader>sl', ':set syn=clojure<cr>')
+-- phaazon/hop.nvim
+key.set('n', 'f', ':HopWord<cr>', {silent = true})
 
 -- vim-expand-region
 key.set('', '+', '<Plug>(expand_region_expand)')
@@ -54,18 +23,3 @@ key.set('', '_', '<Plug>(expand_region_shrink)')
 key.set('n', '<a-j>', ']e', {remap = true})
 key.set('n', '<a-k>', '[e', {remap = true})
 
--- phaazon/hop.nvim
-key.set('n', 'f', ':HopWord<CR>')
-key.set('n', '<leader>hf', ':HopWord<cr>')
-key.set('n', '<leader>h1', ':HopChar1<cr>')
-key.set('n', '<leader>h2', ':HopChar2<cr>')
-key.set('n', '<leader>hl', ':HopLine<cr>')
-
--- Telescope
-key.set('n', '<leader>bo', '<cmd>Telescope find_files hidden=true<cr>')
-key.set('n', '<leader>ff', '<cmd>Telescope find_files hidden=true<cr>')
-key.set('n', '<leader>fg', '<cmd>Telescope live_grep<cr>')
-key.set('n', '<leader>fb', '<cmd>Telescope buffers<cr>')
-key.set('n', '<leader>fh', '<cmd>Telescope help_tags<cr>')
-key.set('n', '<leader>fs', '<cmd>Telescope current_buffer_fuzzy_find<cr>')
-key.set('n', '<leader>fc', '<cmd>Telescope command_history<cr>')
--- config/nvim/lua/plugins.lua
+++ config/nvim/lua/plugins.lua
@@ -14,7 +14,7 @@ Plug 'lewis6991/gitsigns.nvim'
 Plug 'nvim-lua/plenary.nvim' -- dependency of telescope
 Plug 'nvim-telescope/telescope.nvim'
 Plug 'thaerkh/vim-workspace'
-Plug 'liuchengxu/vim-which-key'
+Plug 'folke/which-key.nvim'
 Plug 'mbbill/undotree'
 Plug 'terryma/vim-expand-region'
 Plug 'kyazdani42/nvim-web-devicons' -- file icons
@@ -22,6 +22,16 @@ Plug 'kyazdani42/nvim-tree.lua'
 Plug 'lukas-reineke/indent-blankline.nvim' -- show | every 4 spaces
 Plug 'sbdchd/neoformat'
 vim.call('plug#end')
+
+require'which-key'.setup {}
+require'nvim-web-devicons'.setup {}
+require'hop'.setup()
+require'nvim-tree'.setup {}
+require'feline'.setup()
+require'bufferline'.setup {}
+require'nvim_comment'.setup()
+require'gitsigns'.setup()
+require'indent_blankline'.setup {}
 
 vim.cmd [[
     colorscheme PaperColor
@@ -33,7 +43,7 @@ global.neoformat_basic_format_align=1
 global.neoformat_basic_format_retab=1
 global.neoformat_basic_format_trim=1
 
-home=os.getenv( "HOME" )
+local home=os.getenv( "HOME" )
 global.workspace_create_new_tabs=0
 global.workspace_session_directory=home..'/.config/nvim/sessions/'
 global.workspace_session_disable_on_args=1
@@ -44,11 +54,66 @@ global.workspace_autosave_always=1
 global.undotree_WindowLayout=2
 global.undotree_SetFocusWhenToggle=1
 
-require'nvim-web-devicons'.setup {}
-require'hop'.setup()
-require'nvim-tree'.setup {}
-require'feline'.setup()
-require'bufferline'.setup {}
-require'nvim_comment'.setup()
-require'gitsigns'.setup()
-require'indent_blankline'.setup {}
+local keymap={
+    k = {'<cmd>bnext<cr>', 'buffer-next'},
+    j = {'<cmd>bprevious<cr>', 'buffer-previous'},
+    q = {'<cmd>qa!<cr>', 'quit'},
+    u = {'<cmd>UndotreeToggle<cr>', 'undo-tree-toggle'},
+    t = {'<cmd>NvimTreeToggle<cr>', 'file-tree-toggle'},
+    o = {
+        name = '+OPTIONS',
+        r = {'<cmd>so $MYVIMRC<cr>', 'config-reload'},
+        f = {'<cmd>Neoformat<cr>', 'autoformat'},
+        w = {'<cmd>ToggleWorkspace<cr>', 'workspace-toggle'},
+        a = {'<cmd>ToggleAutosave<cr>', 'autosave-toggle'},
+        s = {'<cmd>setlocal spell! spelllang=en_us<cr>', 'spellchecker-toggle'},
+    },
+    b = {
+        name = '+BUFFER',
+        w = {'<cmd>w!<cr>', 'write'},
+        o = {'<cmd>Telescope find_files hidden=true<cr>', 'open'},
+        x = {'<cmd>bp <bar> bd! #<cr>', 'close'},
+        n = {'<cmd>enew<cr>', 'new'},
+    },
+    h = {
+        name = '+HOP',
+        f = {'<cmd>HopWord<cr>', 'word'},
+        l = {'<cmd>HopLine<cr>', 'line'},
+        s = {'<cmd>HopChar1<cr>', 'single-char'},
+        t = {'<cmd>HopChar2<cr>', 'two-chars'},
+    },
+    f = {
+        name = '+FIND',
+        f = {'<cmd>Telescope find_files hidden=true<cr>', 'file'},
+        g = {'<cmd>Telescope live_grep<cr>', 'grep-live'},
+        b = {'<cmd>Telescope buffers<cr>', 'buffers'},
+        h = {'<cmd>Telescope help_tags<cr>', 'help'},
+        s = {'<cmd>Telescope current_buffer_fuzzy_find<cr>', 'search-fuzzy'},
+        c = {'<cmd>Telescope command_history<cr>', 'command-history'},
+    },
+    w = {
+        name = '+WINDOW',
+        q = {'<c-w>q', 'close'},
+        s = {'<cmd>split<cr>', 'split-horizontal'},
+        v = {'<cmd>vsplit<cr>', 'split-vertical'},
+        h = {'<c-w>h', 'focus-left'},
+        j = {'<c-w>j', 'focus-down'},
+        k = {'<c-w>k', 'focus-up'},
+        l = {'<c-w>l', 'focus-right'},
+        o = {'<cmd>only<cr>', 'only-current-window'},
+    },
+    s = {
+        name = '+SYNTAX',
+        c = {'<cmd>set syn=cs<cr>', 'c#'},
+        f = {'<cmd>set syn=fsharp<cr>', 'f#'},
+        j = {'<cmd>set syn=javascript<cr>', 'js'},
+        t = {'<cmd>set syn=typescript<cr>', 'ts'},
+        s = {'<cmd>set syn=json<cr>', 'json'},
+        x = {'<cmd>set syn=xml<cr>', 'xml'},
+        b = {'<cmd>set syn=sh<cr>', 'bash'},
+        m = {'<cmd>set syn=terrafrom<cr>', 'terraform'},
+        r = {'<cmd>set syn=rust<cr>', 'rust'},
+        l = {'<cmd>set syn=clojure<cr>', 'clojure'},
+    }
+}
+require'which-key'.register(keymap, { prefix = "<leader>" })
