1a08accccfe965f16f309c75c1409eb69f425152
Bartek Łukasik <Nexilis@users.noreply.github.com>
2022-11-16 14:10:55 +0100

    Flatpak apps: -drawing, +mindustry

--- bootstrap/_flatpak-games.sh
+++ bootstrap/_flatpak-games.sh
@@ -7,5 +7,6 @@ flatpak install flathub org.wesnoth.Wesnoth -y
 flatpak install flathub org.openttd.OpenTTD -y
 flatpak install flathub org.develz.Crawl -y
 flatpak install flathub org.gnome.Mines -y
+flatpak install flathub com.github.Anuken.Mindustry -y
 
 flatpak install flathub io.github.antimicrox.antimicrox -y
