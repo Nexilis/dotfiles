0294b7b168f581ba5494de326c6c27e7bfd49bb2
Bartek Łukasik <Nexilis@users.noreply.github.com>
2023-07-26 10:50:57 +0200

    Remove not used apps

--- bootstrap/_flatpak-work.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-flatpak install flathub com.slack.Slack -y
-flatpak install flathub us.zoom.Zoom -y
-flatpak install flathub rest.insomnia.Insomnia -y
-flatpak install flathub com.google.Chrome -y
-
