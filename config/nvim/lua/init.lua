08ea0cc6aa5bcdf50f90d9e662cc8f458d4b3cd4
Bartek Łukasik <Nexilis@users.noreply.github.com>
2023-11-27 14:43:26 +0100

    refresh nvim plugin config & more

--- config/nvim/lua/init.lua
+++ config/nvim/lua/init.lua
@@ -5,205 +5,202 @@ key = vim.keymap
 au = vim.api.nvim_create_autocmd
 home = os.getenv("HOME")
 
-set.dir=home..'/.config/nvim/swap//'
-set.backupdir=home..'/.config/nvim/backup//'
-set.undodir=home..'/.config/nvim/undo//'
-set.backup=true
-set.swapfile=true
-set.undofile=true
-set.termguicolors=true
-set.encoding='utf-8'
-set.langmenu='en_US.utf-8'
-set.background='dark'
-set.list=true
+set.dir = home .. '/.local/state/nvim/swap//'
+set.backupdir = home .. '/.local/state/nvim/backup//'
+set.undodir = home .. '/.local/state/nvim/undo//'
+set.backup = true
+set.swapfile = true
+set.undofile = true
+set.termguicolors = true
+set.encoding = 'utf-8'
+set.langmenu = 'en_US.utf-8'
+set.background = 'dark'
+set.list = true
 set.listchars = {tab = '▸ ', trail = '·'}
-set.mousehide=true
-set.mouse='a'
-set.wrap=false
-set.backspace='2'
-set.ignorecase=true
-set.smartcase=true
-set.tabstop=4
-set.shiftwidth=4
-set.softtabstop=4
-set.expandtab=true
-set.colorcolumn='120'
-set.completeopt='menuone,noinsert,noselect'
+set.mousehide = true
+set.mouse = 'a'
+set.wrap = true
+set.backspace = '2'
+set.ignorecase = true
+set.smartcase = true
+set.tabstop = 4
+set.shiftwidth = 4
+set.softtabstop = 4
+set.expandtab = true
+set.colorcolumn = '120'
+set.completeopt = 'menuone,noinsert,noselect'
 set.formatoptions:remove('t')
 set.path:append('**')
 set.wildignore:append('*/tmp/*,*/cache/*,*.so,*.swp,*.')
 set.shortmess:append('c')
-set.number=true
-set.timeoutlen=500
-set.clipboard='unnamed,unnamedplus'
-set.guifont='Hack Nerd Font Mono:h11'
+set.number = true
+set.timeoutlen = 500
+set.clipboard = 'unnamed,unnamedplus'
+set.guifont = 'Hack Nerd Font Mono:h14' -- overwritten by fontsize.nvim plugin
+set.inccommand = 'split' -- show live preview of search-replace
 
-g.mapleader=' '
-g.maplocalleader=','
+g.loaded_netrw = 1
+g.loaded_netrwPlugin = 1
 
+g.mapleader = ' '
+g.maplocalleader = ','
 
 -- ctrl+z, ctrl+v
-key.set('i', '<c-z>' ,'<c-o>:u<cr>', {silent = true})
-key.set('i', '<c-v>', '<esc>:set paste<cr>a<c-r>=getreg(\'+\')<cr><esc>:set nopaste<cr>a', {silent = true})
+key.set('i', '<c-z>', '<c-o>:u<cr>', {silent = true})
+key.set('i', '<c-v>',
+        '<esc>:set paste<cr>a<c-r>=getreg(\'+\')<cr><esc>:set nopaste<cr>a',
+        {silent = true})
 
 -- delete without copying
 key.set('n', 'x', '"_x')
 key.set({'n', 'v'}, 'd', '"_d')
 key.set('n', 'D', '"_D')
 
-au('BufWritePost', {
-  pattern = 'init.lua',
-  command = 'source <afile> | PackerCompile',
-})
-
-au({'BufRead', 'BufNewFile'}, {
-  pattern = '*.csx',
-  callback = function() vim.bo.filetype = 'cs' end,
-})
+au({'BufRead', 'BufNewFile'},
+   {pattern = '*.csx', callback = function() vim.bo.filetype = 'cs' end})
 
 au('TextYankPost', {
-  pattern = '*',
-  callback = function() vim.highlight.on_yank({higroup="IncSearch", timeout=150}) end,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({higroup = "IncSearch", timeout = 150})
+    end
 })
 
 vim.cmd([[
     au BufReadPost * if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g`\"" | endif
 ]])
 
-local packerPath = fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(packerPath)) > 0 then
-    packer_bootstrap = fn.system({
-        'git',
-        'clone',
-        '--depth',
-        '1',
-        'https://github.com/wbthomason/packer.nvim',
-        packerPath,
-    })
-    print 'Installing packer. If problems restart and run :PackerSync.'
+local function bootstrap_lazy()
+    local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+    if not vim.loop.fs_stat(lazypath) then
+        vim.fn.system({
+            "git", "clone", "--filter=blob:none",
+            "https://github.com/folke/lazy.nvim.git", "--branch=stable", -- latest stable release
+            lazypath
+        })
+    end
+    vim.opt.rtp:prepend(lazypath)
 end
 
-return require('packer').startup(function(use)
-    use ({'wbthomason/packer.nvim',
-        config = function()
-            g.plug_shallow=1
-        end
-    })
+bootstrap_lazy()
 
-    use ({'nvim-lualine/lualine.nvim',
-        config = function()
-            require('lualine').setup({
-                options = {
-                    section_separators = { left = '', right = '' },
-                    component_separators = { left = '', right = '' },
-                    globalstatus = true
-                }
-            })
-        end,
-        requires = {
-            'kyazdani42/nvim-web-devicons'
+require("lazy").setup({
+    'famiu/bufdelete.nvim', -- delete buffers without wiping layout
+    'dstein64/nvim-scrollview', -- scrollbar
+    'vim-scripts/VimCompletesMe',
+    'RRethy/vim-illuminate',
+    {
+        'roobert/search-replace.nvim',
+        opts = {}
+    },
+    {
+        'nvim-lualine/lualine.nvim',
+        opts = { options = {
+            section_separators = {left = '', right = ''},
+            component_separators = {left = '', right = ''},
+            globalstatus = true
+        }},
+        dependencies = {'nvim-tree/nvim-web-devicons'}
+    }, {
+        'akinsho/bufferline.nvim',
+        opts = {},
+        dependencies = {'nvim-tree/nvim-web-devicons'}
+    }, {
+        'folke/tokyonight.nvim',
+        lazy = false,
+        priority = 1000,
+        opts = {},
+        config = function() vim.cmd [[colorscheme tokyonight-night]] end
+    }, {
+        'folke/flash.nvim', -- improved movements
+        event = "VeryLazy",
+        opts = {},
+        keys = {
+            {
+                "s",
+                mode = {"n", "x", "o"},
+                function() require("flash").jump() end,
+                desc = "Flash"
+            }, {
+                "S",
+                mode = {"n", "x", "o"},
+                function() require("flash").treesitter() end,
+                desc = "Flash Treesitter"
+            }, {
+                "r",
+                mode = "o",
+                function() require("flash").remote() end,
+                desc = "Remote Flash"
+            }, {
+                "R",
+                mode = {"o", "x"},
+                function() require("flash").treesitter_search() end,
+                desc = "Treesitter Search"
+            }, {
+                "<c-s>",
+                mode = {"c"},
+                function() require("flash").toggle() end,
+                desc = "Toggle Flash Search"
+            }
         }
-    })
-
-    use ({'famiu/bufdelete.nvim',}) -- delete buffers without wiping layout
-
-    use ({'akinsho/bufferline.nvim',
-        config = function()
-            require('bufferline').setup()
-        end,
-        requires = {
-            'kyazdani42/nvim-web-devicons'
-        }
-    })
-
-    use ({'NLKNguyen/papercolor-theme',
-        config = function()
-            vim.cmd [[
-                colorscheme PaperColor
-            ]]
-        end
-    })
-
-    use ({'phaazon/hop.nvim',
-        config = function()
-            require('hop').setup()
-            key.set('n', 'f', ':HopWord<cr>', {silent = true})
-        end
-    })
-
-    use ({'ajh17/VimCompletesMe',})
-
-    use ({'terrortylor/nvim-comment', -- gcc to toggle comment
+    }, {
+        'terrortylor/nvim-comment', -- gcc to toggle comment
         config = function()
             require('nvim_comment').setup()
         end
-    })
-
-    use ({'tpope/vim-unimpaired', -- toggles yoh, yob, yow, yos
+    }, {
+        'tpope/vim-unimpaired', -- toggles yoh, yob, yow, yos
         config = function()
             -- lines moving
             key.set('n', '<a-j>', ']e', {remap = true})
             key.set('n', '<a-k>', '[e', {remap = true})
         end
-    })
-
-    use ({'lewis6991/gitsigns.nvim',
-        config = function()
-            require('gitsigns').setup()
-        end,
-        requires = {
-            "nvim-lua/plenary.nvim"
-        }
-    })
-
-    use ({
+    }, {
+        'lewis6991/gitsigns.nvim',
+        config = function() require('gitsigns').setup() end,
+        dependencies = {"nvim-lua/plenary.nvim"}
+    }, {
         'nvim-telescope/telescope.nvim',
         config = function()
             require('telescope').setup({
-              pickers = {
-                find_files = {
-                  find_command = { 'fd', '-H', '-S', '-512k', '--type', 'f', '-E', '.cache', '-E', 'flatpak', '-E', '.rustup', '-E', '.steam', '-E', '.mozilla' }
+                pickers = {
+                    find_files = {
+                        find_command = {
+                            'fd', '-H', '-S', '-512k', '--type', 'f', '-E',
+                            '.cache', '-E', 'flatpak', '-E', '.rustup', '-E',
+                            '.steam', '-E', '.mozilla', '-E', '.m2', '-E', '.git', '-E', '.vscode'
+                        }
+                    }
                 }
-              }
             })
         end,
-        requires = {
-            "nvim-lua/plenary.nvim"
-        }
-    })
-
-    use ({'Shatur/neovim-session-manager',
+        event = "VeryLazy",
+        dependencies = {"nvim-lua/plenary.nvim"}
+    }, {
+        'Shatur/neovim-session-manager',
         config = function()
             local smc = require('session_manager.config')
-            require('session_manager').setup({autoload_mode = smc.AutoloadMode.CurrentDir})
-        end
-    })
-
-    use ({'Sup3Legacy/fontsize.nvim',
+            require('session_manager').setup({
+                autoload_mode = smc.AutoloadMode.CurrentDir
+            })
+        end,
+        dependencies = {
+            'stevearc/dressing.nvim'
+        }
+    }, {
+        'Sup3Legacy/fontsize.nvim',
         config = function()
             require('fontsize').init({
                 -- Required argument
                 font = "Hack Nerd Font Mono",
                 -- Optional arguments
                 min = 6,
-                default = 11,
-                max = 24,
-                step = 3 })
+                default = 14,
+                max = 22,
+                step = 4
+            })
         end
-    })
-
-    use ({'itchyny/vim-cursorword', })
-
-    use ({'windwp/nvim-spectre',
-        config = function()
-            require('spectre').setup()
-        end,
-        requires = {
-            "nvim-lua/plenary.nvim"
-        }
-    })
-
-    use ({
+    }, {
         'folke/which-key.nvim',
         config = function()
             local wkey = require('which-key')
@@ -216,12 +213,14 @@ return require('packer').startup(function(use)
                 u = {'<cmd>UndotreeToggle<cr>', 'undo-tree-toggle'},
                 t = {'<cmd>NvimTreeToggle<cr>', 'file-tree-toggle'},
                 l = {'<cmd>nohl<cr>', 'clear-highlight'},
-                r = {'<cmd>lua require("spectre").open_visual({select_word=true})<cr>', 'search-replace'},
+                r = {'<cmd>SearchReplaceSingleBufferOpen<cr>', 'search-replace'},
                 o = {
                     name = '+OPTIONS',
                     r = {'<cmd>so $MYVIMRC<cr>', 'config-reload'},
-                    f = {'<cmd>Neoformat<cr>', 'autoformat'},
-                    s = {'<cmd>setlocal spell! spelllang=en_us<cr>', 'spellchecker-toggle'},
+                    s = {
+                        '<cmd>setlocal spell! spelllang=en_us<cr>',
+                        'spellchecker-toggle'
+                    },
                     ['1'] = {'<cmd>set syn=cs<cr>', 'c#'},
                     ['2'] = {'<cmd>set syn=fs<cr>', 'f#'},
                     ['3'] = {'<cmd>set syn=js<cr>', 'js'},
@@ -231,31 +230,28 @@ return require('packer').startup(function(use)
                     ['7'] = {'<cmd>set syn=sh<cr>', 'bash'},
                     ['8'] = {'<cmd>set syn=terrafrom<cr>', 'terraform'},
                     ['9'] = {'<cmd>set syn=rust<cr>', 'rust'},
-                    ['0'] = {'<cmd>set syn=clojure<cr>', 'clojure'},
+                    ['0'] = {'<cmd>set syn=clojure<cr>', 'clojure'}
                 },
                 b = {
                     name = '+BUFFER',
                     n = {'<cmd>enew<cr>', 'new'},
-                    o = {'<cmd>Telescope find_files<cr>', 'open'},
+                    o = {'<cmd>NvimTreeToggle<cr>', 'open'},
+                    f = {'<cmd>Telescope find_files<cr>', 'find-file'},
                     w = {'<cmd>w!<cr>', 'write'},
                     x = {'<cmd>Bdelete<cr>', 'close'},
                     s = {'<cmd>SessionManager load_session<cr>', 'sessions'},
                     c = {'<cmd>Telescope buffers<cr>', 'current'},
-                    r = {'<cmd>Telescope oldfiles<cr>', 'recent'},
-                },
-                h = {
-                    name = '+HOP',
-                    f = {'<cmd>HopWord<cr>', 'word'},
-                    l = {'<cmd>HopLine<cr>', 'line'},
-                    s = {'<cmd>HopChar1<cr>', 'single-char'},
-                    t = {'<cmd>HopChar2<cr>', 'two-chars'},
+                    r = {'<cmd>Telescope oldfiles<cr>', 'recent'}
                 },
                 s = {
                     name = '+SEARCH',
-                    b = {'<cmd>Telescope current_buffer_fuzzy_find<cr>', 'buffer (fuzzy)'},
+                    b = {
+                        '<cmd>Telescope current_buffer_fuzzy_find<cr>',
+                        'buffer (fuzzy)'
+                    },
                     e = {'<cmd>Telescope live_grep<cr>', 'everywhere (grep)'},
                     h = {'<cmd>Telescope help_tags<cr>', 'help'},
-                    c = {'<cmd>Telescope command_history<cr>', 'commands'},
+                    c = {'<cmd>Telescope command_history<cr>', 'commands'}
                 },
                 w = {
                     name = '+WINDOW',
@@ -266,67 +262,102 @@ return require('packer').startup(function(use)
                     j = {'<c-w>j', 'focus-down'},
                     k = {'<c-w>k', 'focus-up'},
                     l = {'<c-w>l', 'focus-right'},
-                    o = {'<cmd>only<cr>', 'only-current-window'},
+                    o = {'<cmd>only<cr>', 'only-current-window'}
                 },
                 f = {
                     name = '+FONT',
                     ['='] = {'<cmd>FontIncrease<cr>', 'size-up'},
                     ['-'] = {'<cmd>FontDecrease<cr>', 'size-down'},
-                    ['0'] = {'<cmd>FontReset<cr>', 'reset'},
-                },
+                    ['0'] = {'<cmd>FontReset<cr>', 'reset'}
+                }
             }
-            wkey.register(keymap, { prefix = "<leader>" })
+            wkey.register(keymap, {prefix = "<leader>"})
         end
-    })
-
-    use ({'mbbill/undotree',
+    }, {
+        'mbbill/undotree',
         config = function()
-            g.undotree_WindowLayout=2
-            g.undotree_SetFocusWhenToggle=1
+            g.undotree_WindowLayout = 4
+            g.undotree_SetFocusWhenToggle = 1
         end
-    })
-
-    use ({'terryma/vim-expand-region',
+    }, {
+        'terryma/vim-expand-region',
         config = function()
             key.set('', '+', '<Plug>(expand_region_expand)')
             key.set('', '_', '<Plug>(expand_region_shrink)')
         end
-    })
-
-    use ({'stevearc/dressing.nvim', -- better default picker (used by neovim-session-manager)
+    }, {
+        'nvim-tree/nvim-tree.lua',
         config = function()
-            require('dressing').setup({})
-        end
-    })
+            local HEIGHT_RATIO = 0.8
+            local WIDTH_RATIO = 0.8
 
-    use ({'dstein64/nvim-scrollview',}) -- scrollbar
-
-    use ({'kyazdani42/nvim-tree.lua',
-        config = function()
-            local nt = require('nvim-tree')
-            nt.setup({update_focused_file = { enable = true }})
+            require('nvim-tree').setup({
+                view = {
+                    float = {
+                        enable = true,
+                        open_win_config = function()
+                            local screen_w = vim.opt.columns:get()
+                            local screen_h =
+                                vim.opt.lines:get() - vim.opt.cmdheight:get()
+                            local window_w = screen_w * WIDTH_RATIO
+                            local window_h = screen_h * HEIGHT_RATIO
+                            local window_w_int = math.floor(window_w)
+                            local window_h_int = math.floor(window_h)
+                            local center_x = (screen_w - window_w) / 2
+                            local center_y =
+                                ((vim.opt.lines:get() - window_h) / 2) -
+                                    vim.opt.cmdheight:get()
+                            return {
+                                border = 'rounded',
+                                relative = 'editor',
+                                row = center_y,
+                                col = center_x,
+                                width = window_w_int,
+                                height = window_h_int
+                            }
+                        end
+                    },
+                    width = function()
+                        return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+                    end
+                },
+                update_focused_file = {enable = true}
+            })
         end,
-        requires = {
-            'kyazdani42/nvim-web-devicons'
+        event = "VeryLazy",
+        dependencies = {'nvim-tree/nvim-web-devicons'}
+    }, {
+        'lukas-reineke/indent-blankline.nvim', -- show | every 4 spaces
+        event = "VeryLazy",
+        config = function()
+            require("ibl").setup()
+        end
+    }, {
+        'folke/noice.nvim',
+        --event = 'VeryLazy',
+        opts = {
+              lsp = {
+                -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+                override = {
+                  ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                  ["vim.lsp.util.stylize_markdown"] = true,
+                  ["cmp.entry.get_documentation"] = true,
+                },
+              },
+              -- you can enable a preset for easier configuration
+              presets = {
+                bottom_search = false, -- use a classic bottom cmdline for search
+                command_palette = true, -- position the cmdline and popupmenu together
+                long_message_to_split = true, -- long messages will be sent to a split
+                inc_rename = false, -- enables an input dialog for inc-rename.nvim
+                lsp_doc_border = false, -- add a border to hover docs and signature help
+              }
+        },
+        dependencies = {
+            'MunifTanjim/nui.nvim', -- UI Component Library for Neovim, e.g. Layout, Popup, Input, etc.
+            'rcarriga/nvim-notify' -- A fancy, configurable, notification manager for NeoVim
+
         }
-    })
-
-    use ({'lukas-reineke/indent-blankline.nvim', -- show | every 4 spaces
-        config = function()
-            require('indent_blankline').setup()
-        end
-    })
-
-    use ({'sbdchd/neoformat',
-        config = function()
-            g.neoformat_basic_format_align=1
-            g.neoformat_basic_format_retab=1
-            g.neoformat_basic_format_trim=1
-        end
-    })
-
-    if packer_bootstrap then
-        require('packer').sync()
-    end
-end)
+    }
+})
 
